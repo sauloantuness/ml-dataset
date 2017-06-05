8baca6db1274c392607f9e8e590b618c38b8f094
Saulo Antunes <sauloantuness@gmail.com>
2017-06-05 08:53:25 -0300

    Add notebook

--- db2csv.py
+++ db2csv.py
@@ -5,9 +5,16 @@ from datetime import datetime
 conn = sqlite3.connect('dataset.db')
 c = conn.cursor()
 
-c.execute('''SELECT * from solution''')
-for problem, user, date in c.fetchall():
+print 'user,problem,category,date'
+
+c.execute('''
+	SELECT s.user_id, s.problem_id, p.category_id, s.date
+	FROM solution s LEFT JOIN problem p 
+	ON s.problem_id = p.problem_id
+''')
+
+for user, problem, category, date in c.fetchall():
 	d = datetime.strptime(date,'%m/%d/%y, %I:%M:%S %p')
-	print str(user) + ',' + str(problem) + ',' + d.strftime('%m/%d/%y')
+	print str(user) + ',' + str(problem) + ',' + str(category) + ',' + d.strftime('%y/%m/%d %I:%M')
 
 conn.close()
